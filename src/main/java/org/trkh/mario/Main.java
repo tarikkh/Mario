3274643a0f69093b17e6427184ecadc6a49aabf0
= <=>
2024-10-19 14:54:12 +0100

    Add initial project structure
    This commit includes the basic project setup with for
    source code, configuration file.
    
    - Create Maven Project
    - Add The LWJGL library to pom.xml
    - Created Window class to manage GLFW window setup and OpenGL context.
    - Added rendering loop to clear the screen with a red background.
    - Included input handling to close the window when the escape key is pressed.

---
+++ src/main/java/org/trkh/mario/Main.java
@@ -0,0 +1,15 @@
+package org.trkh.mario;
+
+import org.trkh.mario.jade.Window;
+
+/**
+ *
+ * @author tarik khoumri (trkh)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       Window window = Window.get();
+       window.run();
+    }
+}
