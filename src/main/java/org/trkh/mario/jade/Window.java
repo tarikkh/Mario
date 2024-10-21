5c1e702133e644a01b51b1e4205467cf42904025
= <=>
2024-10-21 23:20:23 +0100

    Add Key Listener and Mouse Listener

--- src/main/java/org/trkh/mario/jade/Window.java
+++ src/main/java/org/trkh/mario/jade/Window.java
@@ -1,30 +1,35 @@
 package org.trkh.mario.jade;
 
 import org.lwjgl.Version;
-
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
 import static org.lwjgl.glfw.GLFW.GLFW_MAXIMIZED;
 import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
 import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
 import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
 import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
 import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetCursorPosCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetMouseButtonCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetScrollCallback;
 import static org.lwjgl.glfw.GLFW.glfwShowWindow;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
 import static org.lwjgl.glfw.GLFW.glfwWindowHint;
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
-
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 /**
@@ -36,12 +41,16 @@ public class Window {
     private String title;
     private static Window window = null;
     private long glfwWindow;
+    private float r, g, b, a;
 
     private Window() {
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
-
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+        this.a = 0;
     }
 
     public static Window get() {
@@ -55,56 +64,85 @@ public class Window {
 
         init();
         loop();
-    
+
+        // Free Memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and the free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+
     }
+
     private void init() {
-        //Setup an error callback
-       GLFWErrorCallback.createPrint(System.err).set();
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
 
-       // Initialize GLFW
-       if (!glfwInit()) {
-        throw new IllegalStateException("Unable to Initialize GLFW.");
-       }
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to Initialize GLFW.");
+        }
 
-       //Configure GLFW
-       glfwDefaultWindowHints();
-       glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-       glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-       glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 
-       //Create the window
-       //create number to where adress of the window in memory
-       glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        // Create the window
+        // create number to where adress of the window in memory
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
 
-       if(glfwWindow == NULL){
-        throw new IllegalStateException("Failed to create the GLFW window.");
-       }
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
 
-       //Make the OpenGl context current
-       glfwMakeContextCurrent(glfwWindow);
-       //Enable v-sync
-       glfwSwapInterval(1);
-       //Make the window visible
-       glfwShowWindow(glfwWindow);
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
 
-       /*This line is critical for LWJGL's interoperation with GLFW's
-		OpenGL context, or any context that is managed externally.
-		LWJGL detects the context that is current in the current thread,
-		creates the GLCapabilities instance and makes the OpenGL
-		bindings available for use.
-        */
+        // Make the OpenGl context current
+        glfwMakeContextCurrent(glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
 
-       GL.createCapabilities();
+        /*
+         * This line is critical for LWJGL's interoperation with GLFW's
+         * OpenGL context, or any context that is managed externally.
+         * LWJGL detects the context that is current in the current thread,
+         * creates the GLCapabilities instance and makes the OpenGL
+         * bindings available for use.
+         */
+
+        GL.createCapabilities();
     }
 
     private void loop() {
-        //while the window open
-        while(!glfwWindowShouldClose(glfwWindow)){
-            //poll events
+        // while the window open
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                this.r = 1;
+                this.g = 1;
+                this.b = 1;
+                this.a = 1;
+                glClearColor(r, g, b, a);
+                glClear(GL_COLOR_BUFFER_BIT);
+            }
+
+            this.r = 0;
+            this.g = 0;
+            this.b = 0;
+            this.a = 0;
 
             glfwSwapBuffers(glfwWindow);
         }
